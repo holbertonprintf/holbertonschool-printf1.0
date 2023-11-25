97bf750e6cac0a3e562b3623d5f1f4317e5c87ea
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-11-25 10:29:40 -0800

    added by structure specifiers with alias spc

--- main.h
+++ main.h
@@ -2,6 +2,17 @@
 #define MAIN_H
 #include <stdarg.h>
 
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	void (*f)(va_list args);
+} spc;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 
